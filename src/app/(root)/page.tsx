1a26ba33b3a200cd1ef3449da83e603131048800
kishan <guptakishan492@gmail.com>
2024-12-16 15:15:07 +0530

    bug

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,9 +4,14 @@ import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
+interface SearchParamProps {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
 const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
+
 
   const images = await getAllImages({ page, searchQuery})
 
