1f07e54f0fa8ef899be21d787705baa7bccd8ee2
kishan <guptakishan492@gmail.com>
2024-12-24 00:34:24 +0530

    removing searchparamprops

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,14 +4,7 @@ import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
-interface SearchParamProps {
-  searchParams: {
-    page?: string;
-    query?: string;
-  };
-}
-
-const Home = async ({ searchParams }: SearchParamProps) => {
+const Home = async ({ searchParams }: any) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
